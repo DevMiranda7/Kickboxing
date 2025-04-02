7adaacdc1b5df005362e9a86c8a5c2e787f89e16
d0uglas77p <douglas_1313@hotmail.com>
2025-04-02 18:29:53 -0300

    Tratando pagina inicial

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -1,3 +1,26 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slide");
+
+    if (n >= slides.length) slideIndex = 0;
+    if (n < 0) slideIndex = slides.length - 1;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+
+    slides[slideIndex].classList.add("active");
+}
+
+// Troca automática a cada 5 segundos
+setInterval(() => changeSlide(1), 7000);
+
 document.addEventListener("DOMContentLoaded", function () {
     const modalidades = {
         "btn-modalidade-light": "cabecalho-light",
