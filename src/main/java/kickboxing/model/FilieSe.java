7594945bcf557f3991290ac90a059499c30ef1f4
d0uglas77p <douglas_1313@hotmail.com>
2025-04-15 11:57:24 -0300

    Back end de envio do formulario p/ email

---
+++ src/main/java/kickboxing/model/FilieSe.java
@@ -0,0 +1,73 @@
+package kickboxing.model;
+
+public class FilieSe {
+    private String nomeFilie;
+
+    private String emailFilie;
+
+    private String enderecoFilie;
+
+    private String cidadeFilie;
+
+    private String graduacaoFilie;
+
+    private String contatoFilie;
+
+    private String descricaoFilie;
+
+    public String getNomeFilie() {
+        return nomeFilie;
+    }
+
+    public void setNomeFilie(String nomeFilie) {
+        this.nomeFilie = nomeFilie;
+    }
+
+    public String getEmailFilie() {
+        return emailFilie;
+    }
+
+    public void setEmailFilie(String emailFilie) {
+        this.emailFilie = emailFilie;
+    }
+
+    public String getEnderecoFilie() {
+        return enderecoFilie;
+    }
+
+    public void setEnderecoFilie(String enderecoFilie) {
+        this.enderecoFilie = enderecoFilie;
+    }
+
+    public String getCidadeFilie() {
+        return cidadeFilie;
+    }
+
+    public void setCidadeFilie(String cidadeFilie) {
+        this.cidadeFilie = cidadeFilie;
+    }
+
+    public String getGraduacaoFilie() {
+        return graduacaoFilie;
+    }
+
+    public void setGraduacaoFilie(String graduacaoFilie) {
+        this.graduacaoFilie = graduacaoFilie;
+    }
+
+    public String getContatoFilie() {
+        return contatoFilie;
+    }
+
+    public void setContatoFilie(String contatoFilie) {
+        this.contatoFilie = contatoFilie;
+    }
+
+    public String getDescricaoFilie() {
+        return descricaoFilie;
+    }
+
+    public void setDescricaoFilie(String descricaoFilie) {
+        this.descricaoFilie = descricaoFilie;
+    }
+}
